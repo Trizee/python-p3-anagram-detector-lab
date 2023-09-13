244e19187a7bc1064697faa4b0dde3c386d7250c
Tri Hoang <trihoang09@gmail.com>
2023-09-13 13:59:36 -0600

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self,word):
+        self.word = word
+
+    def match(self, array):
+        arr = []
+        for i in array:
+            if sorted(i) == sorted(self.word):
+                arr.append(i)
+                print(arr)
+        return arr
+
+    
+listen = Anagram('listen')
+
+listen.match(['enlists', 'silent', 'inlets', 'banana'])
